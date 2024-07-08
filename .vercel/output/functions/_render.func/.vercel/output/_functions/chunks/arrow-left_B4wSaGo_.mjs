ea3778fb6f088a3e4ff8b1b9c465ae053e72fc2c
carlos-burelo <carlosfernandoburelo@gmail.com>
2024-07-08 17:51:51 -0600

    :bug: remove unnecesary files

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/arrow-left_B4wSaGo_.mjs
+++
@@ -1,3 +0,0 @@
-const arrowLeft = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\"\r\n    stroke-linejoin=\"round\">\r\n    <line x1=\"5\" y1=\"12\" x2=\"19\" y2=\"12\"></line>\r\n    <polyline points=\"12 5 19 12 12 19\"></polyline>\r\n</svg>";
-
-export { arrowLeft as default };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/learning-js_CUeHP-QY.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./learning-js_tG9FuTrZ.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
